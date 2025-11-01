865a62cbbca7d3d3b43ba9ea3f3801d908a4c590
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-10-31 21:50:51 -0300

    tarefas estão sendo mostradas por meio do li de maneira correta e o input ta sendo atualizado, preciso de uma funcao add

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function App(){
+
+const [tasks, settarefas] = useState<any>([])
+const [input, setInput] = useState<any>("")
+
+
+function add(){
+
+}
+
+
+
+
+
+
+return (
+    <section>
+<h1>Lista de Tarefas</h1>
+<input type="text" value={input} onChange={(e) =>{console.log(e.target.value) 
+    setInput(e.target.value)}}/>
+<button onClick={add}>Add</button>    
+  <ul>
+    {tasks.map((task: any) => (
+        <li key={task}>
+            {task}
+            </li>
+    ))}
+
+  </ul>
+  </section>
+
+)
+
+}
