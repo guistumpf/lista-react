9b077fb6e01d36cf9dc2388f344997f7f441c7f5
João Guilherme Stumpf Fernandes <guist4@proton.me>
2025-10-31 21:59:00 -0300

    formatei o codigo ( a lista ta funcional igual as que eu faço em js puro, porém eu vou fazer dessa o meu magnum opus, preciso adicionar botão de marcar como feito, deletar e editar)

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,40 @@
 import { useState } from "react";
 
-export default function App(){
+export default function App() {
 
-const [tasks, settarefas] = useState<any>([])
-const [input, setInput] = useState<any>("")
-
-
-function add(){
-if(input.trim() === ""){
-    alert("Digite Uma Tarefa")
-    return
-}
+    const [tasks, settarefas] = useState<any>([])
+    const [input, setInput] = useState<any>("")
 
 
+    function add() {
+        if (input.trim() === "") {
+            alert("Digite Uma Tarefa")
+            return
+        }
+        settarefas([...tasks, input])
+        setInput("")
 
-    settarefas([...tasks, input])
-setInput("")
-
-}
+    }
 
 
+    return (
+        <section>
+            <h1>Lista de Tarefas</h1>
+            <input type="text" value={input} onChange={(e) => {
+                console.log(e.target.value)
+                setInput(e.target.value)
+            }} />
+            <button onClick={add}>Add</button>
+            <ul>
+                {tasks.map((task: any) => (
+                    <li key={task}>
+                        {task}
+                    </li>
+                ))}
 
+            </ul>
+        </section>
 
-
-
-return (
-    <section>
-<h1>Lista de Tarefas</h1>
-<input type="text" value={input} onChange={(e) =>{console.log(e.target.value) 
-    setInput(e.target.value)}}/>
-<button onClick={add}>Add</button>    
-  <ul>
-    {tasks.map((task: any) => (
-        <li key={task}>
-            {task}
-            </li>
-    ))}
-
-  </ul>
-  </section>
-
-)
+    )
 
 }
